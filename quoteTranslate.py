63997cab89fde5e2dc24d8779e0233696005b7fd
juanRuiz <juanca.ruiz2010@gmail.com>
2025-03-24 16:35:20 -0400

    added my quote, radio group, and more

--- quoteTranslate.py
+++ quoteTranslate.py
@@ -1,6 +1,16 @@
 import flet as ft
 
 def main(page: ft.Page):
-    pass
+    
+    quote = ft.Text("The rest is silence.")
+    radio_group = ft.RadioGroup(
+        content= ft.Column(
+            controls=[
+                ft.Radio(label="Spanish", value= "El resto es silencio."),
+                ft.Radio(label="Hebrew", value= "השאר זה שקט"),
+                ft.Radio(label="French", value= "Le reste est silence.")
+            ]
+        )
+    )
 
 ft.app(target=main,assets_dir="assets")
